6b0f7e30113da5c3be10c36b07996b1585d7e0f0
Kirill Sak <k.sak@vkteam.ru>
2025-09-20 17:28:49 +0300

    lab3 ended

--- lab3/helpers/calc.py
+++ lab3/helpers/calc.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from typing import Optional
+import numpy
 
 
 class DFParams:
@@ -8,6 +9,8 @@ class DFParams:
     def __init__(self) -> None:
         self._x_bar: Optional[float] = None
         self._y_bar: Optional[float] = None
+        self._Sxx: any = None
+        self._Sxy: any = None 
 
 
     def x_bar(self, tx_column: Optional[float]) -> float:
@@ -18,11 +21,27 @@ class DFParams:
             self._x_bar = float(tx_column.mean())
         return self._x_bar
 
-    @property
+
     def y_bar(self, ty_column: Optional[float]) -> float:
         """Среднее значение Y (ȳ)."""
         if self._y_bar is None:
-            if self._y is None:
+            if ty_column is None:
                 raise ValueError("Данные для Y не предоставлены")
             self._y_bar = float(ty_column.mean())
         return self._y_bar
+
+    def Sxx(self, tx_column: Optional[float]) -> float:
+        """Cкорректированная сумма квадратов для признака x"""
+        if self._Sxx is None:
+            if tx_column is None and self.x_bar is None:
+                raise ValueError("Данные для Sxx не предоставлены")
+            self._Sxx =  numpy.sum((tx_column - self.x_bar) ** 2)
+        return self._Sxx
+
+    def Sxy(self, tx_column: Optional[float], ty_column: Optional[float]) -> float:
+        """Cкорректированная совместная сумма:"""
+        if self._Sxy is None:
+            if tx_column is None and self.y_bar is None and self.y_bar is None:
+                raise ValueError("Данные для Sxy не предоставлены")
+            self._Sxy = numpy.sum((tx_column - self.x_bar) * (ty_column - self.y_bar))
+        return self._Sxy
