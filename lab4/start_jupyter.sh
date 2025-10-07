d82b4a4610222563143e340d3f018046f7eea80e
Semion Voevoda <siamion.vayavoda@pynest.io>
2025-10-07 14:28:26 +0300

    feat: add lab4

---
+++ lab4/start_jupyter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# start_jupyter.sh - Скрипт для запуска Jupyter Notebook
+
+echo "Активация виртуального окружения..."
+source myenv/bin/activate
+
+echo "Запуск Jupyter Notebook..."
+jupyter notebook
+
+
